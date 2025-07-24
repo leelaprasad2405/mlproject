09154d30a46d4bdf56c74c05cf354b8b85d8fa31
Leela <leelaprasad2405@gmail.com>
2025-07-24 12:39:26 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,29 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()  #exc_info gives details of error like in which file it occured, in which line. All these will be shown in the third parameter so we are just considering that and the first two parameters it gives are not that important
+    file_name=exc_tb.tb_frame.f_code.co_filename  #all these details we can find in exception code of python document in google
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+
+    return error_message
+    
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
+
+
+# if __name__=="__main__":
+
+#     try:
+#         a=1/0
+#     except Exception as e:
+#         logging.info("Divide by Zero")
+#         raise CustomException(e,sys)
